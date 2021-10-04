bcd6e0f150f05bec61ad33a44c3282d86f9fe635
Rahul Kandalam <rahulkandalam@gmail.com>
2021-10-04 22:33:58 +0530

    Update Test_fyers_api_v1.py

--- Test_fyers_api_v1.py
+++ Test_fyers_api_v1.py
@@ -2,17 +2,17 @@
 # pip3 install fyers-apiv2
 
 # AUTHORIZATION:
-from fyers_api import fyersModel
-from fyers_api import accessToken
+# from fyers_api import fyersModel
+# from fyers_api import accessToken
 
-client_id = '9BPNFGJHZ5-100'
-secret_key = 'LJCTDO2WQY'
-redirect_url = 'https://tradepop.com/TradeDhar/api-login'
+# client_id = '9BPNFGJHZ5-100'
+# secret_key = 'LJCTDO2WQY'
+# redirect_url = 'https://tradepop.com/TradeDhar/api-login'
 
-session=accessToken.SessionModel(client_id=client_id, secret_key=secret_key, redirect_uri=redirect_url, response_type='code', grant_type='authorization_code')
+# session=accessToken.SessionModel(client_id=client_id, secret_key=secret_key, redirect_uri=redirect_url, response_type='code', grant_type='authorization_code')
 
-response = session.generate_authcode()
-print(response)
+# response = session.generate_authcode()
+# print(response)
 
 #scope=”The value in scope must be openid if being passed.
 #Though this is an optional field”
