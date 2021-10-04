2e761242917b69e1cee1bbe3f6459d547a6b187e
Rahul Kandalam <rahulkandalam@gmail.com>
2021-10-05 01:33:09 +0530

    Update Test_fyers_api_v1.py

--- Test_fyers_api_v1.py
+++ Test_fyers_api_v1.py
@@ -83,7 +83,7 @@ fyers = fyersModel.FyersModel(client_id=client_id, token=access_token)
 # is_async = True  #(By default the async will be False, Change to True for async API calls.)
 
 data = {
-      "symbol":"MCX:SILVERMIC20NOVFUT",
+      "symbol":"NSE:BANKNIFTY20O0737000PE",
       "qty":1,
       "type":1,
       "side":1,
@@ -101,7 +101,7 @@ profilex = fyers.place_order(data)
 print(profilex)
 
 data2 = {
-      "symbol":"MCX:SILVERMIC20OCTFUT",
+      "symbol":"NSE:BANKNIFTY20O0737000PE",
       "qty":1,
       "type":1,
       "side":1,
