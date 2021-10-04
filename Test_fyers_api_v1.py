9f7f06d91136da5d3de978d0639dd81319a4905d
Rahul Kandalam <rahulkandalam@gmail.com>
2021-10-05 03:58:38 +0530

    Update Test_fyers_api_v1.py

--- Test_fyers_api_v1.py
+++ Test_fyers_api_v1.py
@@ -6,14 +6,14 @@ from fyers_api import fyersModel
 from fyers_api import accessToken
 import requests
 
-client_id = '9BPNFGJHZ5-100'
+cid = '9BPNFGJHZ5-100'
 secret_key = 'LJCTDO2WQY'
 redirect_url = 'https://tradepop.com/TradeDhar/api-login'
 response_type = 'code'
 state = 'none'
 API_ENDPOINT = 'https://api.fyers.in/api/v2/generate-authcode?client_id=9BPNFGJHZ5-100&redirect_uri=https%3A%2F%2Ftradepop.com%2FTradeDhar%2Fapi-login&response_type=code&state=None'
 
-session=accessToken.SessionModel(client_id=client_id, secret_key=secret_key, redirect_uri=redirect_url, response_type='code', grant_type='authorization_code')
+session=accessToken.SessionModel(client_id=cid, secret_key=secret_key, redirect_uri=redirect_url, response_type='code', grant_type='authorization_code')
 
 response = session.generate_authcode()
 print(response)
@@ -80,7 +80,7 @@ print(' ')
 
   # "You will be provided with the access_token which will have the below shown response" 
 
-fyers = fyersModel.FyersModel(client_id=client_id, token=access_token)
+fyers = fyersModel.FyersModel(client_id=cid, token=access_token)
 
 # is_async = True  #(By default the async will be False, Change to True for async API calls.)
 
